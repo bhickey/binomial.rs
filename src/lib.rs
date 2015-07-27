b75a5e5e7000a7f1c4d7e6da638d09b1a2330d89
Stu Black <trurl@freeshell.org>
2015-07-26 20:52:41 -0400

    Benchmarks in. Use `cargo build`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+#![feature(test)]
+
+extern crate rand;
+extern crate test;
+
 use std::cmp::Ordering;
 use std::collections::VecDeque;
 use std::fmt;
@@ -46,7 +51,7 @@ struct NodeData {
 type Node = Box<NodeData>;
 
 fn combine(mut h1: Node, h2: Node) -> Node {
-    if h1.value <= h2.value {
+    if h1.value >= h2.value {
         h1.rank += 1;
         h1.nodes.push_back(h2);
         h1
@@ -66,7 +71,6 @@ fn merge_nodes(a: &mut VecDeque<Node>, b: &mut VecDeque<Node>) -> VecDeque<Node>
             (Some(h1), Some(h2)) =>
                 match h1.rank.cmp(&h2.rank) {
                     Ordering::Equal => {
-                        println!["node ranks are equal"];
                         let merged = combine(h1, h2);
                         let r = merged.rank;
                         if r != a.back().map(|n| n.rank).unwrap_or(0) {
@@ -135,7 +139,7 @@ fn merge_nodes(a: &mut VecDeque<Node>, b: &mut VecDeque<Node>) -> VecDeque<Node>
                     },
                 },
         }
-    }    
+    }
 }
 
 impl BinomialHeap {
@@ -145,10 +149,10 @@ impl BinomialHeap {
 
     pub fn push(&mut self, value: i32) {
         let mut v = VecDeque::new();
-        v.push_back(Box::new(NodeData { 
-                          rank: 0,            
-                          value: value,       
-                          nodes: VecDeque::new()
+        v.push_back(Box::new(NodeData {
+            rank: 0,
+            value: value,
+            nodes: VecDeque::new()
         }));
         self.heads = merge_nodes(
             &mut self.heads,
@@ -161,7 +165,7 @@ impl BinomialHeap {
         }
         let mut min_idx = 0usize;
         for (i, node) in self.heads.iter().enumerate() {
-            if node.value < self.heads[min_idx].value {
+            if node.value > self.heads[min_idx].value {
                 min_idx = i;
             }
         }
@@ -169,14 +173,14 @@ impl BinomialHeap {
             *self.heads.remove(min_idx).unwrap();
 
         self.heads = merge_nodes(
-          &mut self.heads,
-          &mut nodes);
-        
+            &mut self.heads,
+            &mut nodes);
+
         return Some(value)
     }
 
     pub fn peek(&self) -> Option<i32> {
-        self.heads.iter().map(|n| n.value).min()
+        self.heads.iter().map(|n| n.value).max()
     }
 
     pub fn is_empty(&self) -> bool {
@@ -187,14 +191,97 @@ impl BinomialHeap {
         let mut sz = 0;
         for node in self.heads.iter() {
             match node.rank {
-              0 => sz += 1,
-              x => sz += 2 << (x - 1),
+                0 => sz += 1,
+                x => sz += 2 << (x - 1),
             }
         }
         return sz
     }
 
     pub fn merge(&mut self, mut other: BinomialHeap) {
-      self.heads = merge_nodes(&mut self.heads, &mut other.heads);
+        self.heads = merge_nodes(&mut self.heads, &mut other.heads);
+    }
+}
+
+#[cfg(test)]
+mod mytest {
+    use ::BinomialHeap;
+    use std::clone::Clone;
+    use std::collections::BinaryHeap;
+    use ::test::Bencher;
+    use rand::{Rng, SeedableRng, StdRng};
+
+    #[test]
+    fn instantiate_empty_heap() {
+        BinomialHeap::new();
+    }
+
+    #[test]
+    fn singleton_heap() {
+        let mut t = BinomialHeap::new();
+        assert_eq![t.len(), 0];
+        assert![t.is_empty()];
+        t.push(23i32);
+        assert_eq![t.len(), 1];
+        assert![!t.is_empty()];
+        assert_eq![t.peek(), Some(23i32)];
+        assert_eq![t.pop(), Some(23i32)];
+    }
+
+    fn get_values() -> Vec<i32> {
+        let seed: &[_] = &[1, 2, 3, 4];
+        let mut rng: StdRng = SeedableRng::from_seed(seed);
+        (0..100).map(|_| rng.gen::<i32>()).collect()
+    }
+
+    #[bench]
+    fn bench_binomial(b: &mut Bencher) {
+        let values = get_values();
+        let sorted = {
+            let mut v = values.clone();
+            v.sort_by(|x, y| y.cmp(x));
+            v
+        };
+
+        b.iter(|| {
+            let mut heap = BinomialHeap::new();
+            for v in &values {
+                heap.push(*v);
+            }
+            let mut heap_sorted = Vec::new();
+            heap_sorted.reserve(heap.len());
+            loop {
+                match heap.pop() {
+                    None => break,
+                    Some(x) => heap_sorted.push(x),
+                }
+            }
+            assert_eq![heap_sorted, sorted];
+        });
+    }
+
+    #[bench]
+    fn bench_builtin(b: &mut Bencher) {
+        let values = get_values();
+        let sorted = {
+            let mut v = values.clone();
+            v.sort_by(|x, y| y.cmp(x));
+            v
+        };
+        b.iter(|| {
+            let mut heap = BinaryHeap::new();
+            for v in &values {
+                heap.push(*v);
+            }
+            let mut heap_sorted = Vec::new();
+            heap_sorted.reserve(heap.len());
+            loop {
+                match heap.pop() {
+                    None => break,
+                    Some(x) => heap_sorted.push(x),
+                }
+            }
+            assert_eq![heap_sorted, sorted];
+        });
     }
 }
